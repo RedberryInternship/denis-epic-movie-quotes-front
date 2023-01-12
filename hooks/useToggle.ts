6fc741f7fd6e73d1a592273d0c688ace44488cb6
Denissant <denissanturyan@gmail.com>
2023-01-12 16:14:07 +0400

    chore: update useToggle return type

--- hooks/useToggle.ts
+++ hooks/useToggle.ts
@@ -1,8 +1,8 @@
 import { useCallback, useState } from 'react';
 
-export const useToggle = (initialState = false): [boolean, () => void] => {
+export const useToggle = (initialState = false) => {
   const [state, setState] = useState(initialState);
   const toggle = useCallback(() => setState((state) => !state), []);
 
-  return [state, toggle];
+  return [state, toggle] as const;
 };
