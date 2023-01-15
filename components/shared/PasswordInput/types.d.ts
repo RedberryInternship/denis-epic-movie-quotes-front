62120cbb2fe1c526f45edc53382f32fb37064a2a
Denissant <denissanturyan@gmail.com>
2023-01-15 15:15:21 +0400

    chore: replace any type

--- components/shared/PasswordInput/types.d.ts
+++ components/shared/PasswordInput/types.d.ts
@@ -1,10 +1,12 @@
+import { ChangeEvent } from 'react';
+
 export type PropsType = {
   name: string;
   label: string;
   requiredAsterisk?: boolean;
   placeholder: string;
   validationRules?: object;
-  onChange?: (arg0: any) => void;
+  onChange?: (arg0: ChangeEvent<HTMLInputElement>) => void;
   isHidden: boolean;
   toggleIsHidden: () => void;
 };
