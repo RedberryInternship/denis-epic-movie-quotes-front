00ca69b465567e748fcc49e5a5e630bb80eaef62
Denissant <denissanturyan@gmail.com>
2023-01-16 17:18:22 +0400

    feat: send locale to backend

--- components/shared/GoogleAuthButton/useGoogleAuthButton.ts
+++ components/shared/GoogleAuthButton/useGoogleAuthButton.ts
@@ -1,5 +1,8 @@
+import { useTranslation } from 'next-i18next';
+
 export const useGoogleAuthButton = () => {
+  const { i18n } = useTranslation();
   return () => {
-    window.location.href = `${process.env.NEXT_PUBLIC_API_URL}/oauth/redirect`;
+    window.location.href = `${process.env.NEXT_PUBLIC_API_URL}/oauth/redirect?locale=${i18n.language}`;
   };
 };
--- services/axios.ts
+++ services/axios.ts
@@ -1,4 +1,6 @@
-import axios, { AxiosResponse } from 'axios';
+import axios, { AxiosHeaders, AxiosRequestConfig, AxiosResponse } from 'axios';
+import { i18n } from 'next-i18next';
+import i18nextConfig from '../next-i18next.config';
 
 const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
@@ -9,6 +11,14 @@ const instance = axios.create({
 });
 
 instance.defaults.withCredentials = true;
+
+instance.interceptors.request.use((config: AxiosRequestConfig) => {
+  (config.headers as AxiosHeaders).set(
+    'Accept-Language',
+    i18n?.language ?? i18nextConfig.i18n.defaultLocale
+  );
+  return config;
+});
 
 instance.interceptors.response.use(
   async (response: AxiosResponse) => {
