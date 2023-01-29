634183dd7e9c4eb6d0f9242489ef84fd713bf9a2
Denissant <denissanturyan@gmail.com>
2023-01-29 19:44:09 +0400

    refactor: move logout logic to navbar

--- components/shared/Navbar/Navbar.tsx
+++ components/shared/Navbar/Navbar.tsx
@@ -5,11 +5,14 @@ import {
   LanguageSelector,
   Search,
 } from 'components';
+import { useNavbar } from './useNavbar';
 
 const Navbar = (props: {
-  logout: () => Promise<void>;
   toggleSideMenuIsOpen: () => void;
+  displaySearchButton: boolean;
 }) => {
+  const { logout } = useNavbar();
+
   return (
     <header className='flex justify-between items-center h-22 bg-brand-lightmodal px-9 lg:px-17.5'>
       <div className='hidden lg:block'>
@@ -19,15 +22,17 @@ const Navbar = (props: {
         <Burger />
       </button>
       <div className='flex gap-5 items-center'>
-        <div className='lg:hidden w-6 h-6'>
-          <Search />
-        </div>
+        {props.displaySearchButton && (
+          <div className='lg:hidden w-6 h-6'>
+            <Search />
+          </div>
+        )}
         <Bell />
         <div className='hidden lg:flex text-white'>
           <LanguageSelector />
           <button
             className='ml-4 h-9.5 px-6.5 border border-white rounded'
-            onClick={props.logout}
+            onClick={logout}
           >
             Log out
           </button>
