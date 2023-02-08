dca29bc707f20ae89c7779eeceee34f7226bb69a
Denissant <denissanturyan@gmail.com>
2023-02-08 18:21:24 +0400

    style: improve form error animation

--- components/shared/FormError.tsx
+++ components/shared/FormError.tsx
@@ -7,7 +7,9 @@ const FormError = (props: { error: string; animated?: boolean }) => {
     <p
       className={
         'text-brand-crimson text-sm ' +
-        (props.error && props.animated ? 'h-[1.25rem] mb-4 lg:mb-0' : '') +
+        (props.error && props.animated
+          ? 'h-[1.25rem] -mt-3 mb-7 lg:mb-0'
+          : '') +
         (props.animated
           ? 'transition-all duration-200 h-0 '
           : 'min-h-[1.25rem]')
