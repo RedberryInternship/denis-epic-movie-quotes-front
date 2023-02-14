c8a9b6400ea7248638d6c14bb17a284fb980170d
Denissant <denissanturyan@gmail.com>
2023-02-14 23:42:16 +0400

    style: add scroll snapping to landing page

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,10 @@ import i18nextConfig from '../next-i18next.config';
 
 export default function Document(props: DocumentProps) {
   return (
-    <Html lang={props.__NEXT_DATA__.locale ?? i18nextConfig.i18n.defaultLocale}>
+    <Html
+      className='snap-y snap-mandatory'
+      lang={props.__NEXT_DATA__.locale ?? i18nextConfig.i18n.defaultLocale}
+    >
       <Head></Head>
       <body>
         <Main />
