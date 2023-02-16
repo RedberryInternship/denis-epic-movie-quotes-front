886a3a741d3659983785c26c3b942864e2b63e51
Denissant <denissanturyan@gmail.com>
2023-02-16 13:24:37 +0400

    style: change landing modal close button color

--- components/shared/CloseModalButton.tsx
+++ components/shared/CloseModalButton.tsx
@@ -4,7 +4,7 @@ const CloseModalButton = (props: { onClick: () => void }) => {
   return (
     <button
       onClick={props.onClick}
-      className='fixed lg:absolute bg-[#00000080] p-3 z-50 opacity-80 top-10 left-5.5 lg:bg-transparent lg:left-auto lg:right-8.5 rounded-full'
+      className='text-white fixed lg:absolute bg-[#00000080] p-3 z-50 opacity-80 top-10 left-5.5 lg:bg-transparent lg:left-auto lg:right-8.5 rounded-full'
     >
       <Close />
     </button>
