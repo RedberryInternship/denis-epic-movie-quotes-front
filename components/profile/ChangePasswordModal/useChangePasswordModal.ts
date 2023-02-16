7c348afbaab35b36869d7c06c0d82ab2b1b60bcc
Denissant <denissanturyan@gmail.com>
2023-02-16 13:06:26 +0400

    feat: add password validity hint

--- components/profile/ChangePasswordModal/useChangePasswordModal.ts
+++ components/profile/ChangePasswordModal/useChangePasswordModal.ts
@@ -1,6 +1,7 @@
 import { useProfileModalSubmit, useToggle } from 'hooks';
 import { sendUpdateProfileRequest } from 'services';
 import { SetState } from 'types';
+import { useWatch } from 'react-hook-form';
 
 export const useChangePasswordModal = (
   setPasswordModalIsOpen: SetState<boolean>
@@ -14,11 +15,17 @@ export const useChangePasswordModal = (
 
   const [passwordIsHidden, togglePasswordIsHidden] = useToggle(true);
 
+  const passwordValue = useWatch({ name: 'password' });
+  const passwordMinLengthValid = passwordValue && passwordValue.length >= 8;
+  const passwordMaxLengthValid = passwordValue && passwordValue.length < 16;
+
   return {
     isLoading,
     handleSubmit,
     closeModalCallback,
     passwordIsHidden,
     togglePasswordIsHidden,
+    passwordMinLengthValid,
+    passwordMaxLengthValid,
   };
 };
