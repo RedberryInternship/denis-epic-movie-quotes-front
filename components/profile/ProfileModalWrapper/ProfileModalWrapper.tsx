ac71bb19097446a3a15179609fb1a8f18f8d504d
Denissant <denissanturyan@gmail.com>
2023-02-17 21:13:57 +0400

    style: fix profile modal positions

--- components/profile/ProfileModalWrapper/ProfileModalWrapper.tsx
+++ components/profile/ProfileModalWrapper/ProfileModalWrapper.tsx
@@ -14,6 +14,7 @@ const ProfileModalWrapper = (
     modalRef,
     setFormIsConfirmed,
     setConfirmModalIsOpen,
+    openConfirmIfValid,
   } = useProfileModalWrapper(
     props.closeModalCallback,
     props.isLoading,
@@ -23,37 +24,44 @@ const ProfileModalWrapper = (
   return (
     <>
       {confirmModalIsOpen ? (
-        <div className='absolute z-50 top-22 px-2 w-full bg-brand-lightbackground min-h-screen pt-18 text-white'>
-          <div className='rounded-1.5lg bg-gradient-nested-modal'>
-            <div className='pt-14 pb-11 border-b border-brand-divide px-4 text-center'>
-              Are you sure you want to make these changes?
-            </div>
-            <div className='h-19 flex justify-between items-center pr-7 pl-8'>
-              <button
-                type='button'
-                className='text-brand-pale lg:text-white'
-                onClick={() => setConfirmModalIsOpen(false)}
-              >
-                Cancel
-              </button>
-              <RedButton
-                onClick={() => setFormIsConfirmed(true)}
-                label={props.isLoading ? 'Loading...' : 'Confirm'}
-                classNames='px-4.5'
-              />
+        <>
+          <div className='fixed inset-0 z-[70] bg-gradient-modal opacity-70'></div>
+          <div className='fixed animate-grow z-[100] top-22 pt-38 px-2 w-full bg-brand-lightbackground min-h-screen pt-18 text-white'>
+            <div className='rounded-1.5lg bg-gradient-nested-modal'>
+              <div className='pt-14 pb-11 border-b border-brand-divide px-4 text-center'>
+                Are you sure you want to make these changes?
+              </div>
+              <div className='h-19 flex justify-between items-center pr-7 pl-8'>
+                <button
+                  type='button'
+                  className='text-brand-pale lg:text-white'
+                  onClick={() => setConfirmModalIsOpen(false)}
+                >
+                  Cancel
+                </button>
+                <RedButton
+                  onClick={() => setFormIsConfirmed(true)}
+                  label={props.isLoading ? 'Loading...' : 'Confirm'}
+                  classNames='px-4.5'
+                />
+              </div>
             </div>
           </div>
-        </div>
+        </>
       ) : (
         <div
-          className='absolute pt-40 min-h-screen w-full rounded text-white pointer-events-none flex items-stretch lg:block lg:static lg:p-0 lg:min-h-0 lg:rounded-0 lg:pointer-events-auto'
+          className='fixed z-[100] top-40 min-h-screen w-full rounded text-white pointer-events-none flex items-stretch lg:block lg:static lg:p-0 lg:min-h-0 lg:rounded-0 lg:pointer-events-auto'
           ref={modalRef}
         >
-          <div className='bg-brand-lightbackground w-full lg:bg-transparent z-30'>
+          <button
+            className='absolute z-[120] -top-10 left-8 w-8 h-8 pointer-events-auto'
+            onClick={props.closeModalCallback}
+          ></button>
+          <div className='bg-brand-lightbackground max-h-[calc(100vh-5.5rem)] w-full lg:bg-transparent z-30'>
             <div className='pointer-events-auto h-min text-white w-full flex items-center justify-center'>
               <form
                 onSubmit={props.handleSubmit}
-                className='static overflow-auto max-h-[80vh] min-h-[173px] lg:h-auto lg:fixed z-40 lg:top-[20vh] 2xl:top-[32vh] w-[615px] lg:bg-brand-article rounded-xl p-0 lg:pt-6 lg:pb-10 pb-20'
+                className='static animate-grow overflow-auto max-h-full min-h-[173px] lg:h-auto lg:fixed z-40 lg:top-[20vh] 2xl:top-[32vh] w-[615px] lg:bg-brand-article rounded-xl p-0 lg:pt-6 lg:pb-10 pb-20'
               >
                 <div className='hidden lg:block border-b border-brand-pale'>
                   <h2 className='text-2xl font-medium pr-14 pl-8 pb-6'>
@@ -72,7 +80,7 @@ const ProfileModalWrapper = (
                     Cancel
                   </button>
                   <RedButton
-                    onClick={() => setConfirmModalIsOpen(true)}
+                    onClick={openConfirmIfValid}
                     label='Add'
                     classNames='lg:hidden text-base px-4.5'
                   />
