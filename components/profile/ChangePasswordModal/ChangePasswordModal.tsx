586cdfacbacc0578b0f580b9078b155329f2a390
Denissant <denissanturyan@gmail.com>
2023-02-18 21:35:08 +0400

    refactor: extract password hint component

--- components/profile/ChangePasswordModal/ChangePasswordModal.tsx
+++ components/profile/ChangePasswordModal/ChangePasswordModal.tsx
@@ -1,4 +1,4 @@
-import { PasswordInput, ProfileModalWrapper } from 'components';
+import { PasswordHint, PasswordInput, ProfileModalWrapper } from 'components';
 import { SetState } from 'types';
 import { useChangePasswordModal } from './useChangePasswordModal';
 
@@ -11,8 +11,6 @@ const ChangePasswordModal = (props: {
     closeModalCallback,
     passwordIsHidden,
     togglePasswordIsHidden,
-    passwordMinLengthValid,
-    passwordMaxLengthValid,
     t,
   } = useChangePasswordModal(props.setPasswordModalIsOpen);
 
@@ -31,41 +29,7 @@ const ChangePasswordModal = (props: {
           toggleIsHidden={togglePasswordIsHidden}
           isBig={true}
         />
-        <div className='h-[134px] bg-brand-article p-6 border-[#373741] rounded border mb-8 lg:mb-4 lg:w-1/2 lg:max-w-[528px] mt-2'>
-          {t('password_should_contain')}:
-          <div
-            className={
-              'text-sm mt-4 mb-1 ' +
-              (passwordMinLengthValid ? 'text-white' : 'text-[#9C9A9A]')
-            }
-          >
-            <span
-              className={
-                'mr-1.5 ' +
-                (passwordMinLengthValid ? 'text-brand-green' : 'text-[#9C9A9A]')
-              }
-            >
-              •
-            </span>
-            {t('min_8')}
-          </div>
-          <div
-            className={
-              'text-sm ' +
-              (passwordMaxLengthValid ? 'text-white' : 'text-[#9C9A9A]')
-            }
-          >
-            <span
-              className={
-                'mr-1.5 ' +
-                (passwordMaxLengthValid ? 'text-brand-green' : 'text-[#9C9A9A]')
-              }
-            >
-              •
-            </span>
-            {t('max_15')}
-          </div>
-        </div>
+        <PasswordHint />
         <div className='mb-4'>
           <PasswordInput
             name='password'
--- components/profile/ChangePasswordModal/useChangePasswordModal.ts
+++ components/profile/ChangePasswordModal/useChangePasswordModal.ts
@@ -1,7 +1,6 @@
 import { useProfileModalSubmit, useToggle } from 'hooks';
 import { sendUpdateProfileRequest } from 'services';
 import { SetState } from 'types';
-import { useWatch } from 'react-hook-form';
 import { useTranslation } from 'next-i18next';
 
 export const useChangePasswordModal = (
@@ -18,18 +17,12 @@ export const useChangePasswordModal = (
 
   const [passwordIsHidden, togglePasswordIsHidden] = useToggle(true);
 
-  const passwordValue = useWatch({ name: 'password' });
-  const passwordMinLengthValid = passwordValue && passwordValue.length >= 8;
-  const passwordMaxLengthValid = passwordValue && passwordValue.length < 16;
-
   return {
     isLoading,
     handleSubmit,
     closeModalCallback,
     passwordIsHidden,
     togglePasswordIsHidden,
-    passwordMinLengthValid,
-    passwordMaxLengthValid,
     t,
   };
 };
